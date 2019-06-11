59a5604ac605fd31d7847981de140d04fb168658
bearscho <43170264+bearscho@users.noreply.github.com>
2019-06-11 23:41:02 +0900

    aa

--- firestore_ws.js
+++ firestore_ws.js
@@ -11,9 +11,7 @@ if (platfotm == "win32") {
 }
 
 var munje_id = ""
-
 console.log("%s-%s",platfotm,hostip);
-
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -27,11 +25,8 @@ db.collection("naver_hana_nowmunje").doc("OnXQxdA84H5CYXjH6GBy")
         munje_id = doc.get("munje_id");
         console.log("onSnapshot - 현재 문제 ID => " + munje_id);
 
-        snedBroadMsg("" + munje_id);
+        snedBroadMsg("S2:"+ munje_id);   	//현재문제 ID
 });
-
-
-
 
 var WebSocketServer = require('websocket').server;
 var http = require('http');
@@ -60,19 +55,7 @@ wsServer = new WebSocketServer({
     autoAcceptConnections: false
 });
 
-function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed.
-  return true;
-}
-
-
 wsServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
-    }
 
     var connection = request.accept(null, request.origin);
 
@@ -82,11 +65,41 @@ wsServer.on('request', function(request) {
 
     console.log((new Date()) + ' Connection accepted.');
     connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-                     // 브로드캐스팅!!
-            snedBroadMsg( munje_id);
-        }
+
+			var recvmsg = "";
+	    recvmsg = message.utf8Data;
+			var code = "";
+	    var msg = "";
+	    code = recvmsg.substring(0,2);
+	    msg = recvmsg.substring(3);
+
+        console.log('Received Message: ' + recvmsg);
+
+				if(code == "C1") {
+		      snedBroadMsg("S1:"+ "접속 환영!!!");   	//hello
+					snedBroadMsg("S2:"+ munje_id);   	//현재문제 ID
+		    } else if(code =="C2") {  //현재 문제 ID 응답
+		     	snedBroadMsg("S2:"+ munje_id);   	//현재문제 ID
+				} else if(code =="C3") {  //현재 문제 ID 에 대한하는 id,text 응답
+						get_hanja_info(munje_id,function (res) {
+						console.log(res);
+						snedBroadMsg("S3:"+ JSON.stringify(res));   	//현재문제 ID
+					}	);
+				} else if(code =="C4") {  //다음 문제로 업데이트
+					console.log("다음문제로 업데이트");
+
+					get_munjeCnt(function (res) {
+						console.log(res);
+						get_nextMunje(res,function(res1) {
+								console.log(res1);
+
+								update_db_nextMunje(res1);
+						});
+					}	);
+
+				}
+
+
 
     });
     connection.on('close', function(reasonCode, description) {
@@ -98,4 +111,69 @@ function snedBroadMsg(msg) {
   clients.forEach(function(cli) {
       cli.sendUTF(msg);
   });
+}
+
+
+get_hanja_info = function(munje_id,callback) {
+  console.log("get_hanja_info-",munje_id);
+	var m_id = "";
+	var m_text = "";
+
+  var docRef = db.collection("naver_munje_list").doc(munje_id);
+  docRef.get().then(function(doc) {
+          if (doc.exists) {
+              console.log("get_hanja_info Document data:", doc.data());
+							callback(doc.data());
+          } else {
+              console.log("No such document!");
+          }
+  }).catch(function(error) {
+          console.log("Error getting document:", error);
+  });
+}
+
+
+get_munjeCnt = function(callback) {
+	var MunjeListCount = 0;
+	var munje_listsRef = db.collection("naver_munje_list");
+	munje_listsRef.get().then(snap => {
+		 MunjeListCount = snap.size // will return the collection size
+		 console.log("전체 문제수 : ", MunjeListCount);
+		 callback(MunjeListCount);
+	});
+}
+
+
+get_nextMunje = function(cnt, callback) {
+	MunjeListRandom = Math.floor((Math.random() * cnt));
+  console.log(MunjeListRandom,"====",MunjeListRandom);
+  //갯수 사이의 랜덤숫자 위치에 해당하는 Doc ID를 구함
+  var NextMunjeID = "";
+
+  var first = db.collection("naver_munje_list")
+        .orderBy("hanja_id");
+
+  first.get().then(function (documentSnapshots) {
+    // Get the last visible document
+    var lastVisible = documentSnapshots.docs[MunjeListRandom];
+    console.log("다음 문제", lastVisible.id, lastVisible.data());
+		callback(lastVisible.id);
+	});
+}
+
+update_db_nextMunje = function (hanja_next_munje,callback) {
+   console.log("다음문제 ",hanja_next_munje)
+
+  var nowmunjeRef = db.collection("naver_hana_nowmunje").doc("OnXQxdA84H5CYXjH6GBy");
+  nowmunjeRef.update({
+      munje_id: hanja_next_munje
+  })
+  .then(function() {
+      console.log("다음문제 Document successfully updated!");
+  })
+  .catch(function(error) {
+      // The document probably doesn't exist.
+      console.error("Error updating document: ", error);
+  });
+
 }
